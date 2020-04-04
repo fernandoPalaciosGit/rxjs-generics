9320bffebde7f53003cdd67f17db219d9b303829
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-04-04 17:30:05 +0200

    use expected chai testing

--- tests/array_iterators.specs.js
+++ tests/array_iterators.specs.js
@@ -1,20 +1,24 @@
 import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect } from 'chai';
 import NEW_RELEASES from './mocks/new_releases';
 import NAMES from './mocks/names';
 import MOVIE_LIST from './mocks/movie_lists';
 
 describe('arrays', () => {
     let result;
+    let expected;
 
     beforeEach(() => {
         result = [];
+        expected = [];
     });
 
     afterEach(() => {
-        console.log(result);
+        expect(result).to.be.eql(expected);
     });
 
     it('Print all the names in an array', () => {
+        expected = ['Ben', 'Jafar', 'Matt', 'Priya', 'Brian'];
         NAMES.forEach((name) => result.push(name));
     });
 
@@ -28,7 +32,13 @@ describe('arrays', () => {
         };
     });
 
-    it(' Project an array of videos into an array of {id,title}', () => {
+    it('Project an array of videos into an array of {id,title}', () => {
+        expected = [
+            { id: 70111470, title: 'Die Hard' },
+            { id: 654356453, title: 'Bad Boys' },
+            { id: 65432445, title: 'The Chamber' },
+            { id: 675465, title: 'Fracture' }
+        ];
         result = NEW_RELEASES.map(({ id, title }) => ({ id, title }));
     });
 
@@ -45,16 +55,45 @@ describe('arrays', () => {
     });
 
     it('Use forEach() to collect only those videos with a rating of 5.0', () => {
+        expected = [
+            {
+                id: 654356453,
+                title: 'Bad Boys',
+                boxart: 'http://cdn-0.nflximg.com/images/2891/BadBoys.jpg',
+                uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
+                rating: 5,
+                bookmark: [
+                    {
+                        "id": 432534,
+                        "time": 65876586
+                    }
+                ],
+            },
+            {
+                id: 675465,
+                title: 'Fracture',
+                boxart: 'http://cdn-0.nflximg.com/images/2891/Fracture.jpg',
+                uri: 'http://api.netflix.com/catalog/titles/movies/70111470',
+                rating: 5,
+                bookmark: [
+                    {
+                        "id": 432534,
+                        "time": 65876586
+                    }
+                ]
+            }
+        ];
         result = NEW_RELEASES.filter(({ rating }) => rating === 5.0);
     });
 
-    it(' Chain filter and map to collect the ids of videos that have a rating of 5.0', () => {
+    it('Chain filter and map to collect the ids of videos that have a rating of 5.0', () => {
+        expected = [654356453, 675465];
         result = NEW_RELEASES
             .filter(({ rating }) => rating === 5.0)
             .map(({ id }) => id);
     });
 
     it(' Flatten the movieLists array into an array of video ids', () => {
-        
+
     });
 });
