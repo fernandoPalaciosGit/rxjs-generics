e660f061a5d2896301a77909dae4a34693ea5cae
Fernando Palacios Landi <f.palacios@avoristravel.com>
2020-04-07 20:15:32 +0200

    create map and filter prototype methods

--- app/observable_prototype.js
+++ app/observable_prototype.js
@@ -16,12 +16,35 @@ class Observable {
         }
     }
 
+    map(callbackMapOnNext) {
+        const observableContextPrevious = this;
+        return new Observable(function (observer) {
+            return observableContextPrevious.subscribe( // resuelves el observer enterior de la cola
+                (result) => observer.onNext(callbackMapOnNext(result)), // comunicas al siguiente observer lo que devuelve al resolver el contexto anterior con tu callback
+                (error) => observer.onError(error), // le comunicas el error que rejecta el observer anterior
+                () => observer.onComplete(), // le comunicas que has acabado de resolver el observer amterior
+            )
+        });
+    }
+
+    filter(callbackFilterOnNext) {
+        const observableContextPrevious = this;
+        return new Observable(function (observer) {
+            return observableContextPrevious.subscribe(
+                (result) => callbackFilterOnNext(result) && observer.onNext(result),
+                (error) => observer.onError(error),
+                () => observer.onComplete(),
+            )
+        });
+    }
+
     static fromEvent(domEl, event) {
         return new Observable(function (observer) {
             const handler = (ev) => observer.onNext(ev);
 
             domEl.addEventListener(event, handler);
 
+            // Subscription!!
             return {
                 dispose: () => domEl.removeEventListener(event, handler)
             }
